f365dc7be902ed1283607a67c0f25a17dc0b5250
Swmnx <No>
2021-06-12 21:21:28 +0300

    Task completion

--- paintTask/ViewController.swift
+++ paintTask/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var paintView: UIImageView!
+    var startDrawingPoint: CGPoint?
+    
+    @IBAction func clearPaint(_ sender: UIButton) {
+        paintView.image = nil
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        paintView.backgroundColor  = .systemTeal
     }
-
-
+    
+    @IBAction func handlePan(recognizer: UIPanGestureRecognizer) {
+        let endDrawingPoint = recognizer.location(in: paintView)
+    
+        if let startPoint = startDrawingPoint,
+           recognizer.state == .changed || recognizer.state == .ended {
+            drawLineInPaintView(from: startPoint, to: endDrawingPoint)
+        }
+        
+        startDrawingPoint = endDrawingPoint
+    }
+    
+    private func drawLineInPaintView(from startDrawingPoint: CGPoint, to endDrawingPoint: CGPoint) {
+        UIGraphicsBeginImageContext(paintView.frame.size)
+        paintView.image?.draw(in: paintView.bounds)
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        context.setLineWidth(3)
+        context.setStrokeColor(UIColor.black.cgColor)
+        context.setLineCap(.round)
+        context.move(to: startDrawingPoint)
+        context.addLine(to: endDrawingPoint)
+        context.strokePath()
+        paintView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+    }
 }
-
